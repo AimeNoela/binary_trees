0ccd8255aadf9a4ad311dee0e71d6ffec8f93393
AimeNoela <aimeenoella@gmail.com>
2023-09-01 10:19:46 -0700

    update code

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -6,7 +6,7 @@
  * @tree: Pointer to the root node of the tree to be traversed.
  * @func: Pointer to a function to be applied to each node's value.
  *
- * Description: This function traverses a binary tree using pre-order traversal.
+ * Description: function traverses a binary tree using pre-order traversal.
  * It applies the given function to the value of each node, starting from the
  * root and visiting the left and right subtrees recursively.
  *
@@ -16,12 +16,16 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 	{
-		return; /* If the tree or the function is NULL, no further action is needed*/
+		return;
+		/* If the tree or the function is NULL, no further action is needed*/
 	}
 	else
 	{
-		func(tree->n); /*Apply the function to the current node's value*/
-		binary_tree_preorder(tree->left, func);  /* Recursively traverse the left subtree */
-		binary_tree_preorder(tree->right, func); /* Recursively traverse the right subtree */
+		func(tree->n);
+		/* Apply the function to the current node's value*/
+		binary_tree_preorder(tree->left, func);
+		/*Recursively traverse the left subtree*/
+		binary_tree_preorder(tree->right, func);
+		/* Recursively traverse the right subtree */
 	}
 }
