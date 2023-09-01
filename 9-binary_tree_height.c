5c71f1f07a49f8670b38e4616d2a71eb5ac07b4e
AimeNoela <aimeenoella@gmail.com>
2023-09-01 09:58:15 -0700

    update code

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -29,7 +29,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 			left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 			right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		}
-		/* Return the maximum height of left and right subtrees plus 1 for the current node*/
+		/**
+		 * Return the maximum height of left
+		 * and right subtrees plus 1 for the current node
+		 */
 		return ((left_height > right_height) ? left_height : right_height);
 	}
 }
